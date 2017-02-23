9bc30e95992f7de322334211fa323e629615be30
sconfield <isconfield@icloud.com>
2017-02-23 17:37:45 +0800

    add desktop entrance

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
+import { Window, TitleBar, Toolbar, SearchField } from 'react-desktop/macOs';
 
 function IndexPage() {
+  const doubleClickHandle = e=>{
+    alert("lalala");
+  };
+
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
+      <Window chrome height="400px" width="600px">
+        <TitleBar title="sconfield's iMac" controls>
+          <Toolbar height="43" width="200px" horizontalAlignment="right">
+            <SearchField placeholder="Search" defaultValue="" />
+          </Toolbar>
+        </TitleBar>
+        <div className={styles.file} onDoubleClick={doubleClickHandle}>
+          <div className={styles.fileIcon1}>
+          </div>
+          <label className={styles.fileFont}>zhangjing</label>
+        </div>
+        <div className={styles.file} onDoubleClick={doubleClickHandle}>
+          <div className={styles.fileIcon2}>
+          </div>
+          <label className={styles.fileFont}>张静</label>
+        </div>
+      </Window>
     </div>
   );
 }
